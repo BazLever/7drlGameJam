0237935556ce7b5fb274ff29dccd79dc27dac967
Baz Lever <bazlever6@gmail.com>
2021-03-09 11:55:06 +1100

    Added base for pause menu

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public Animator pauseMenuAnim;
+    bool isPaused;
+
+    void Start()
+    {
+        isPaused = false;   
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                pauseMenuAnim.SetBool("isPaused", true);
+                isPaused = true;
+                Time.timeScale = 0;
+            } else if (isPaused)
+            {
+                pauseMenuAnim.SetBool("isPaused", false);
+                isPaused = false;
+                Time.timeScale = 1;
+            }
+            
+        }
+    }
+}
